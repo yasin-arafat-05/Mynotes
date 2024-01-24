d75d9804e26a9bf4486ceb9f1329ef84e83b3f8b
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2024-01-24 15:47:16 +0600

    java

---
+++ java.java
@@ -0,0 +1,14 @@
+class java {
+    public static void main(String args[]){
+        int myInteger = 42;
+        float myFloat = 3.14f;
+        double myDouble = 6.28d;
+        char myChar = 'A';
+        bool isTrue = true;
+        byte myByte = 127;
+        short myShort = 32000;
+        long myLong = 123456789L;
+        uint myUnsignedInt = 500;
+        ulong myUnsignedLong = 123456789UL;
+    }
+}
